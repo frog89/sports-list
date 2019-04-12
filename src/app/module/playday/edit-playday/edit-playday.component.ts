657055a49604eb710103890915c0f8c6335a5b09
Frank Augustin <augustin.frank@gmail.com>
2019-04-12 16:24:25 +0200

    Styling of reactive form

--- src/app/module/playday/edit-playday/edit-playday.component.ts
+++ src/app/module/playday/edit-playday/edit-playday.component.ts
@@ -46,15 +46,41 @@ export class EditPlaydayComponent implements OnInit {
 
     this.myForm = this.fb.group( {
       dayControl: [this.playday.day, Validators.compose([Validators.required])],
-      playerControl1: [this.playday.playerIds[0]],
-      playerControl2: [this.playday.playerIds[1]],
-      playerControl3: [this.playday.playerIds[2]],
-      playerControl4: [this.playday.playerIds[3]],
-      playerControl5: [this.playday.playerIds[4]]
-    }, {validator: this.differentPlayers() } );
+      isCancelled: [this.playday.isCancelled],
+      numOfHours: [this.playday.numOfHours],
+      numOfCourts: [this.playday.numOfCourts]
+    });
+
+    for (var i: number = 0; i < 10; i++) {
+      var controlName : string = `playerControl${i+1}`;
+      var ctrl : FormControl = new FormControl(null);
+      this.myForm.addControl(controlName, ctrl);
+    }
+
+    this.myForm.validator = Validators.compose([
+      this.valiDifferentPlayers(),
+      this.valiCancelledAndCourtsAndHours(
+        this.myForm.controls.isCancelled,
+        this.myForm.controls.numOfHours,
+        this.myForm.controls.numOfCourts)]);
   }
 
-  differentPlayers() {
+  valiCancelledAndCourtsAndHours(isCancelledCtrl: AbstractControl, 
+      numOfHoursCtrl: AbstractControl, 
+      numOfCourtsCtrl: AbstractControl) {
+    return (group: FormGroup): {[key: string]: any} => {
+      if (isCancelledCtrl.value && numOfHoursCtrl.value > 0)
+        return { invalidCancelledButHoursGreaterZero: true };
+      if (!isCancelledCtrl.value && numOfHoursCtrl.value <= 0)
+        return { invalidNotCancelledButHoursZero: true };
+      if (isCancelledCtrl.value && numOfCourtsCtrl.value > 0) 
+        return { invalidCancelledButCourtsGreaterZero: true };
+      if (!isCancelledCtrl.value && numOfCourtsCtrl.value <= 0)
+        return { invalidNotCancelledButCourtsZero: true };
+    }
+  }
+
+  valiDifferentPlayers() {
     return (group: FormGroup): {[key: string]: any} => {
       let values = [group.controls.playerControl1.value,
         group.controls.playerControl2.value,
@@ -88,7 +114,7 @@ export class EditPlaydayComponent implements OnInit {
       this.myForm.controls[controlName].setValue(playerId);
     }
 
-    this.myForm.valueChanges.subscribe(console.log);
+    //this.myForm.valueChanges.subscribe(console.log);
   }
 
 
@@ -111,8 +137,16 @@ export class EditPlaydayComponent implements OnInit {
    }
 
   onSubmit() {
-    console.log("playerControl1: " + this.myForm.value.playerControl1 + 
-      ", Day: " + this.myForm.value.dayControl);
+    console.log("Day: " + this.myForm.value.dayControl);
+    console.log("playerControl1: " + this.myForm.value.playerControl1);
+    console.log("playerControl2: " + this.myForm.value.playerControl2);
+    console.log("playerControl3: " + this.myForm.value.playerControl3);
+    console.log("playerControl4: " + this.myForm.value.playerControl4);
+    console.log("playerControl5: " + this.myForm.value.playerControl5);
+    console.log("isCancelled: " + this.myForm.value.isCancelled);
+    console.log("numOfHours: " + this.myForm.value.numOfHours);
+    console.log("numOfCourts: " + this.myForm.value.numOfCourts);
+    
   }
 
 
--- src/app/module/playday/playday.module.ts
+++ src/app/module/playday/playday.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { PlaydayRoutingModule } from './playday-routing.module';
 import { PlaydayListComponent } from './playday-list.component';
 import { PlaydayTableComponent } from './playday-table/playday-table.component';
-import { MatTableModule, MatPaginatorModule, MatSortModule, MatFormField, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatOptionModule, MatSelectModule } from '@angular/material';
+import { MatTableModule, MatPaginatorModule, MatSortModule, MatFormField, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatOptionModule, MatSelectModule, MatCheckboxModule, MatGridListModule, MatCardModule } from '@angular/material';
 import { SharedModule } from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { EditPlaydayComponent } from './edit-playday/edit-playday.component';
@@ -26,6 +26,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatOptionModule,
     MatSelectModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatCardModule,
     CommonModule,
     ReactiveFormsModule
   ],
