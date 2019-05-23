1950a7526691722b9ea74b05306c35beab8a7d44
Frank Augustin <augustin.frank@gmail.com>
2019-05-23 20:58:30 +0200

    Renamed Bedminton to sports-list

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/badminton'),
+      dir: require('path').join(__dirname, '../coverage/sports-list'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
